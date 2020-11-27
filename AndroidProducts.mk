64f5baeb5dd44ac29692bc889f8b02a09a24fb3b
Filippo <zzkw35@gmail.com>
2020-11-28 00:21:39 +0100

    guacamole: Adapt to StatiXOS
    Change-Id: I353a635f65113c76eceb40b3972205808a7d1b8d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,9 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The StatiXOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_guacamole.mk
+    $(LOCAL_DIR)/statix_guacamole.mk
